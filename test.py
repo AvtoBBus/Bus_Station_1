de2275bc6bbef9c28bb96143a7ad71fa67a05ba1
AvtoBBus <113329450+AvtoBBus@users.noreply.github.com>
2022-10-23 00:02:49 +0400

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from iterator import Iterator
-
-pipiter = Iterator(
-    10, "C:/Users/miste/OneDrive/Desktop/ВАЖНОЕ/учёба/для лаб/ПП/Bus_Station_1/Bus_Station_1/dataset_another.csv")
-
-for elem in pipiter:
-    print(elem)
