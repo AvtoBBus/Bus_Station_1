5d26da7162910fb11c89a30ebd11d5cd7ada0588
AvtoBBus <113329450+AvtoBBus@users.noreply.github.com>
2022-10-25 14:55:14 +0400

    some design
    Изменён дизайн, стало покрасивее(наверное)

--- main_window.py
+++ main_window.py
@@ -2,6 +2,7 @@ from cgitb import text
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 from PyQt5.QtGui import QPixmap, QFont, QIcon
+from PyQt5.QtCore import Qt
 
 import sys
 import copying_to_another
@@ -28,60 +29,81 @@ class Window(QMainWindow):
         self.label_img_zebra.setFixedSize(500, 400)
         self.label_img_bay_horse.setFixedSize(500, 400)
 
+        self.label_folderpath_text = QLabel(self)
+        self.label_folderpath_text.setFont(QFont("Times", 10))
+        if not self.folderpath_dataset == "":
+            self.label_folderpath_text.setText(
+                "Выбранный путь до dataset: " + self.folderpath_dataset)
+        else:
+            self.label_folderpath_text.setText(
+                "Выбранный путь до dataset: NONE")
+        self.label_folderpath_text.setStyleSheet("color: rgb(200, 200, 200);")
+        self.label_folderpath_text.adjustSize()
+
         self.info_text_label = QLabel(self)
         self.info_text_label.setFont(self.font_in_label)
 
-        self.setWindowTitle("Лапка")
+        self.setWindowTitle("Лапка 3 ПП")
         self.setFixedSize(1280, 720)
-        self.move(250, 250)
+        self.move(320, 180)
 
         self.button_create_annotation = QtWidgets.QPushButton(self)
         self.button_create_annotation.setFont(self.font_in_label)
         self.button_create_annotation.setText(
             "Создать аннотацию к dataset")
         self.button_create_annotation.setGeometry(
-            340, 50, self.x_size_label, self.y_size_label)
+            340, 60, self.x_size_label, self.y_size_label)
         self.button_create_annotation.setIcon(
             QIcon("button_icons\csv_icon.png"))
+        self.button_create_annotation.setStyleSheet(
+            "background: rgb(255, 24, 24);")
 
         self.button_create_another = QtWidgets.QPushButton(self)
         self.button_create_another.setFont(self.font_in_label)
         self.button_create_another.setText(
             "Создать копию dataset с другими названиями")
         self.button_create_another.setGeometry(
-            340, 90, self.x_size_label, self.y_size_label)
+            340, 100, self.x_size_label, self.y_size_label)
         self.button_create_another.setIcon(
             QIcon("button_icons\create_to_another.png"))
+        self.button_create_another.setStyleSheet(
+            "background: rgb(255, 179, 24);")
 
         self.button_create_with_new_number = QtWidgets.QPushButton(self)
         self.button_create_with_new_number.setFont(self.font_in_label)
         self.button_create_with_new_number.setText(
             "Создать копию dataset со случайными номерами")
         self.button_create_with_new_number.setGeometry(
-            340, 130, self.x_size_label, self.y_size_label)
+            340, 140, self.x_size_label, self.y_size_label)
         self.new_text = QtWidgets.QLabel(self)
         self.button_create_with_new_number.clicked.connect(
             self.create_new_number)
         self.button_create_with_new_number.setIcon(
             QIcon("button_icons\create_with_new_number.png"))
+        self.button_create_with_new_number.setStyleSheet(
+            "background: rgb(148, 255, 24);")
 
         self.button_next_bay_horse = QtWidgets.QPushButton(self)
         self.button_next_bay_horse.setFont(self.font_in_label)
         self.button_next_bay_horse.setText(
             "Следующая картинка c гнедой лошадью")
         self.button_next_bay_horse.setGeometry(
-            340, 170, self.x_size_label, self.y_size_label)
+            340, 180, self.x_size_label, self.y_size_label)
         self.button_next_bay_horse.setIcon(
             QIcon("button_icons/next_bay_horse.png"))
+        self.button_next_bay_horse.setStyleSheet(
+            "background: rgb(24, 101, 255);")
 
         self.button_next_zebra = QtWidgets.QPushButton(self)
         self.button_next_zebra.setFont(self.font_in_label)
         self.button_next_zebra.setText(
             "Следующая картинка c зеброй")
         self.button_next_zebra.setGeometry(
-            340, 210, self.x_size_label, self.y_size_label)
+            340, 220, self.x_size_label, self.y_size_label)
         self.button_next_zebra.setIcon(
             QIcon("button_icons/next_zebra.png"))
+        self.button_next_zebra.setStyleSheet(
+            "background: rgb(225, 24, 255);")
 
         self.button_next_zebra.clicked.connect(self.next_zebra)
         self.button_next_bay_horse.clicked.connect(self.next_bay_horse)
